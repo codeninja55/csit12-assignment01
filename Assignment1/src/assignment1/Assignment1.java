afba60b0081b64c137528d81246d5ee9587399d5
Andrew Che <andrew@codeninja55.me>
2017-08-01 02:35:49 +1000

    Added Assignment1 files from Netbeans

---
+++ Assignment1/src/assignment1/Assignment1.java
@@ -0,0 +1,23 @@
+package assignment1;
+
+/**
+ *
+ * @author Dinh Che
+ * Student Number: 5721970
+ * Email: dbac496@uowmail.edu.au
+ */
+public class Assignment1 {
+    
+    static private ArrayList<Card> cards;
+    static private ArrayList<Purchase> purchases;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    } // end of main method
+    
+    public static void makePurchase()
+    {
+        // ...
+    } // end of makePurchase method
+    
+} // end of Assignment1 class
